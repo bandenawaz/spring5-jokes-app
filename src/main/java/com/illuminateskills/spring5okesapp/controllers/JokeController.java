536ad08aa0296c491fdb9de62d8f4877f64f4f25
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-08-13 16:02:05 +0530

    created template to display joke. Closes #9

--- src/main/java/com/illuminateskills/spring5okesapp/controllers/JokeController.java
+++ src/main/java/com/illuminateskills/spring5okesapp/controllers/JokeController.java
@@ -2,16 +2,18 @@ package com.illuminateskills.spring5okesapp.controllers;
 
 import com.illuminateskills.spring5okesapp.services.JokesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class JokeController {
 
     private final JokesService jokesService;
 
-    @Autowired
+
+
     public JokeController(JokesService jokesService) {
         this.jokesService = jokesService;
     }
--- src/main/java/com/illuminateskills/spring5okesapp/services/JokesServiceImpl.java
+++ src/main/java/com/illuminateskills/spring5okesapp/services/JokesServiceImpl.java
@@ -1,16 +1,19 @@
 package com.illuminateskills.spring5okesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
-
+//@NoArgsConstructor(force = true)
 public class JokesServiceImpl implements JokesService {
 
     private  final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
 
